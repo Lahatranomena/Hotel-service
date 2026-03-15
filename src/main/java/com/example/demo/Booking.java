825a63138e3d8f799509b2faf28e91ac8bb2a19e
Lahatranomena <hei.lahatra.3@gmail.com>
2026-03-15 20:39:27 +0300

    feat: add Booking model

---
+++ src/main/java/com/example/demo/Booking.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+public class Booking {
+    private String customerName;
+    private String phoneNumber;
+    private String email;
+    private int roomNumber;
+    private String roomDescription;
+    private String date;
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getRoomDescription() {
+        return roomDescription;
+    }
+
+    public void setRoomDescription(String roomDescription) {
+        this.roomDescription = roomDescription;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
